c4c24b09a5ba4baf6fea9ac7539051426f71c376
nda111 <nda111@naver.com>
2020-11-23 11:54:57 +0900

    Rename: util.js -> utils.js

--- js/utils.js
+++ js/utils.js
@@ -11,6 +11,34 @@ function degrees(radian) {
     return radian * 180.0 / Math.PI;
 }
 
+/**
+ * Mix the two color with their ratio.
+ * 
+ * @param {*} c1 
+ * @param {*} c2 
+ * @param {*} rate 
+ */
+function mixColor(c1, c2, rate) {
+
+    console.assert(0 <= rate && rate <= 1);
+
+    const _rate = 1 - rate;
+
+    r1 = (c1 >> 16) & 0xFF;
+    g1 = (c1 >> 8) & 0xFF;
+    b1 = c1 & 0xFF;
+
+    r2 = (c2 >> 16) & 0xFF;
+    g2 = (c2 >> 8) & 0xFF;
+    b2 = c2 & 0xFF;
+
+    r3 = r1 * rate + r2 * _rate;
+    g3 = g1 * rate + g2 * _rate;
+    b3 = b1 * rate + b2 * _rate;
+
+    return (r3 << 16) | (g3 << 8) | b3;
+}
+
 // Load an .obj file with a texture in image format.
 function loadObject(objectPath, texturePath, onLoad, onError) {
 
@@ -38,4 +66,12 @@ function loadObject(objectPath, texturePath, onLoad, onError) {
             onLoad(objects);
         }, undefined, onError);
     }, undefined, onError);
+}
+
+// Load an .glb file and execute onLoad(gltf), onError(error)
+// gltf.scene -> model, gltf.animations -> animations
+function loadGLB(glbPath, onLoad, onError) {
+
+    const loader = new THREE.GLTFLoader();
+    loader.load(glbPath, onLoad, undefined, onError);
 }
